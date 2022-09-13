15e9fbfe8114ca1accf6c62a03fb05b3163d0be8
dindin12138 <din12138@outlook.com>
2022-09-13 23:42:58 +0800

    refactor: this plugin syntax is deprecated, use new style config

--- chadrc.lua
+++ chadrc.lua
@@ -1,48 +1,35 @@
 -- Just an example, supposed to be placed in /lua/custom/
 
 local M = {}
-local override_conf = require('custom.plugins.override_config')
-local user_conf = require('custom.plugins.user_config')
-
--- make sure you maintain the structure of `core/default_config.lua` here,
--- example of changing theme:
+local conf = require('custom.plugins.config')
 
 M.ui = { theme = "onedark" }
 
 M.plugins = {
-    -- Override default config of a plugin
-    override = {
-        ["NvChad/ui"] = override_conf.ui,
-        ["kyazdani42/nvim-tree.lua"] = override_conf.nvim_tree,
-        ["nvim-telescope/telescope.nvim"] = override_conf.telescope,
-        ["nvim-treesitter/nvim-treesitter"] = override_conf.treesitter,
-        ["williamboman/mason.nvim"] = override_conf.mason,
-        ["hrsh7th/nvim-cmp"] = override_conf.cmp,
-        ["goolord/alpha-nvim"] = override_conf.alpha
+    ["NvChad/ui"] = { override_options = conf.ui },
+    ["kyazdani42/nvim-tree.lua"] = { override_options = conf.nvim_tree },
+    ["nvim-telescope/telescope.nvim"] = { override_options = conf.telescope },
+    ["nvim-treesitter/nvim-treesitter"] = { override_options = conf.treesitter },
+    ["williamboman/mason.nvim"] = { override_options = conf.mason },
+    ["hrsh7th/nvim-cmp"] = { override_options = conf.cmp },
+    ["goolord/alpha-nvim"] = { disable = false, override_options = conf.alpha },
+    ["folke/which-key.nvim"] = { disable = false, },
+    ["rcarriga/nvim-notify"] = { config = conf.notify },
+    ["dstein64/vim-startuptime"] = { cmd = "StartupTime" },
+    ["phaazon/hop.nvim"] = {
+        branch = 'v2',
+        cmd = { "HopWord", "HopLine", "HopChar1" },
+        config = conf.hop
     },
-    -- Remove plugins
-    remove = {},
-    -- Modify plugin definition options
-    user = {
-        ["folke/which-key.nvim"] = { disable = false, },
-        ["goolord/alpha-nvim"] = { disable = false },
-        ["rcarriga/nvim-notify"] = { config = user_conf.notify },
-        ["dstein64/vim-startuptime"] = { cmd = "StartupTime" },
-        ["phaazon/hop.nvim"] = {
-            branch = 'v2',
-            cmd = { "HopWord", "HopLine", "HopChar1" },
-            config = user_conf.hop
-        },
-        ["neovim/nvim-lspconfig"] = {
-            config = function()
-                require "plugins.configs.lspconfig"
-                require "custom.plugins.lsp"
-            end,
-        },
-        ["ahmedkhalf/project.nvim"] = { cmd = "Telescope", config = user_conf.project },
-        ["ray-x/lsp_signature.nvim"] = { after = "nvim-lspconfig", config = user_conf.lsp_signature },
-        ["stevearc/aerial.nvim"] = { cmd = "AerialToggle", config = user_conf.aerial }
-    }
+    ["neovim/nvim-lspconfig"] = {
+        config = function()
+            require "plugins.configs.lspconfig"
+            require "custom.plugins.lsp"
+        end,
+    },
+    ["ahmedkhalf/project.nvim"] = { cmd = "Telescope", config = conf.project },
+    ["ray-x/lsp_signature.nvim"] = { after = "nvim-lspconfig", config = conf.lsp_signature },
+    ["stevearc/aerial.nvim"] = { cmd = "AerialToggle", config = conf.aerial }
 }
 
 M.mappings = require("custom.mappings")
--- plugins/config.lua
+++ plugins/config.lua
@@ -175,4 +175,68 @@ M.treesitter = {
     },
 }
 
+M.notify = function()
+    require("notify").setup({
+        ---@usage Animation style one of { "fade", "slide", "fade_in_slide_out", "static" }
+        stages = "slide",
+        ---@usage Function called when a new window is opened, use for changing win settings/config
+        on_open = nil,
+        ---@usage Function called when a window is closed
+        on_close = nil,
+        ---@usage timeout for notifications in ms, default 5000
+        timeout = 2000,
+        -- Render function for notifications. See notify-render()
+        render = "default",
+        ---@usage highlight behind the window for stages that change opacity
+        background_colour = "Normal",
+        ---@usage minimum width for notification windows
+        minimum_width = 50,
+        ---@usage Icons for the different levels
+        icons = {
+            ERROR = "",
+            WARN = "",
+            INFO = "",
+            DEBUG = "",
+            TRACE = "✎",
+        },
+    })
+
+    vim.notify = require("notify")
+end
+
+M.hop = function()
+    require('hop').setup { keys = 'etovxqpdygfblzhckisuran' }
+end
+
+M.project = function()
+    require("project_nvim").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        detection_methods = { "pattern" },
+        patterns = {
+            "README.md",
+            "Cargo.toml",
+            ".sln",
+            ".git",
+            "_darcs",
+            ".hg",
+            ".bzr",
+            ".svn",
+            "Makefile",
+        },
+        exclude_dirs = { "~/.local/*" }
+    }
+end
+
+M.lsp_signature = function()
+    require('lsp_signature').setup()
+end
+
+M.aerial = function()
+    require("aerial").setup({
+        min_width = { 25, 0.13 },
+    })
+end
+
 return M
