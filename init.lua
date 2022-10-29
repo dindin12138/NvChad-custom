b7aeac906402349efd27a8eb7028659b2526d2e2
dindin12138 <din12138@outlook.com>
2022-10-29 08:13:32 +0800

    feat: add some options

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- example file i.e lua/custom/init.lua
 -- load your options globals, autocmds here or anything .__.
 -- you can even override default options here (core/options.lua)
+
+vim.opt.scrolloff      = 8;
+vim.opt.sidescrolloff  = 8;
+vim.opt.relativenumber = true;
